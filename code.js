f2c0da8e74f79caf211614d7af5882e1df8099ca
SujitGhaley <SGHALEY86280@uat.edu>
2025-08-15 01:42:14 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -47,3 +47,51 @@
     // this will prevent the form from trying to submit to server side code that we don't have in this class
     return false;
 }
+
+// <img id="memeImage" src="programming-meme-14.jpg" alt="Meme Image">
+    //this variable will keep track of the current interval ID - we are storing this ID so that we can stop the image from moving
+    let intervalID = 0;
+
+    //create a function to generate a random number
+    function getRandomNumber(){
+        //get a random number, multiply it by 1000, round down the number, and return it
+        return Math.floor(Math.random() * 800);
+    } 
+
+    // This function will start the image moving around the page
+    function startMove() {
+        //create a shortcut to the meme image
+        let imgMeme = document.getElementById("memeImage");
+
+        intervalID = setInterval(function(){
+            //create variables to give a new location for the image
+            let newTop = getRandomNumber();
+            let newLeft = getRandomNumber();
+
+            //we are changing the css property for the image - changing from left & top 25%
+            imgMeme.style.left = newLeft + "px";
+            imgMeme.style.top = newTop + "px";
+
+            console.log("left=" + newLeft + "px");
+            console.log("top=" + newTop + "px");
+        }, 1000); // 1000 is in milliseconds - 1000 milliseconds = 1 second
+
+        //set it up so that we cannot click on the start button
+        document.getElementById("btnStart").disabled = true;
+        //set it up so that we cannot click on the stop button
+        document.getElementById("btnStop").disabled = false;
+
+
+    }
+
+    // function to stop the image from moving
+    function stopMove(){
+        //call the built in javascript function to stop the runnning code that we started running in setInterval
+        clearInterval(intervalID);
+
+        //set it up so that we cannot click on the start button
+        document.getElementById("btnStart").disabled = false;
+        //set it up so that we cannot click on the stop button
+        document.getElementById("btnStop").disabled = true;
+
+    }
